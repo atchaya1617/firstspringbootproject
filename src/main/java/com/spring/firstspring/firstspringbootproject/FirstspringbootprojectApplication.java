c5a65b4d7ee8ee365964adb58442d26bfe7eaed0
atchaya1617 <atchayaravi04@gmail.com>
2024-05-09 22:41:39 -0700

    alignment

--- src/main/java/com/spring/firstspring/firstspringbootproject/FirstspringbootprojectApplication.java
+++ src/main/java/com/spring/firstspring/firstspringbootproject/FirstspringbootprojectApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FirstspringbootprojectApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(FirstspringbootprojectApplication.class, args);
-	}
 
+		SpringApplication.run(FirstspringbootprojectApplication.class, args);
+
+	}
 }
